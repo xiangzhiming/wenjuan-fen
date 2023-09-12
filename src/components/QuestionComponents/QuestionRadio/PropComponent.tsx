26e81c9ee94a12ae365b7445b66c994d54d82c44
向治名 <zhiming.xiang@datavaluable.com>
2023-09-12 15:24:54 +0800

    完成QuestionRadio单选组件的右侧属性选项的展示、添加、删除

--- src/components/QuestionComponents/QuestionRadio/PropComponent.tsx
+++ src/components/QuestionComponents/QuestionRadio/PropComponent.tsx
@@ -1,11 +1,11 @@
 import {FC, useEffect} from "react";
-import {QuestionRadioPopsType} from "./interface";
-import {Form, Select} from "antd";
-import Input from "antd/es/input/Input";
+import {OptionType, QuestionRadioPopsType} from "./interface";
+import {Select, Form, Input, Space, Button} from 'antd';
 import Checkbox from "antd/es/checkbox/Checkbox";
+import {MinusCircleOutlined, PlusOutlined} from "@ant-design/icons";
 
 export const PropComponent: FC<QuestionRadioPopsType> = (props: QuestionRadioPopsType) => {
-    const {title, isVertical, value, options = [], onChange, disabled} = props;
+    const {title, isVertical, value, options, onChange, disabled} = props;
     const [form] = Form.useForm();
     useEffect(() => {
         form.setFieldsValue({title, isVertical, value, options})
@@ -23,6 +23,44 @@ export const PropComponent: FC<QuestionRadioPopsType> = (props: QuestionRadioPop
               disabled={disabled}>
             <Form.Item label={"标题"} name={"title"} rules={[{required: true, message: "请输入标题..."}]}>
                 <Input/>
+            </Form.Item>
+            <Form.Item label={"选项"}>
+                <Form.List name={"options"}>
+                    {(fields, {add, remove}) => (<>
+                        {/* 遍历所有的选项（可删除）*/}
+                        {fields.map(({name, key}, index) => {
+                            return (<Space key={key} align={"baseline"}>
+                                {/* 当前选项 */}
+                                <Form.Item name={[name, "text"]} rules={[
+                                    {required: true, message: "请输入选项文字"},
+                                    {
+                                        validator: (_,text) => {
+                                            const {options = []} = form.getFieldsValue();
+
+                                            let num = 0;
+                                            options.forEach( (opt: OptionType)  => {
+                                                if (opt.text === text) num++;  // 记录text相同的个数，预期只有1个，自己
+                                            })
+                                            if (num === 1) return Promise.resolve();
+                                            return Promise.reject(new Error("和其他选项重复了"));
+                                        }
+                                    }
+                                ]}>
+                                    <Input placeholder={"输入选项文字..."}/>
+                                </Form.Item>
+                                {/*当前选项删除按钮*/}
+                                {index > 1 && <MinusCircleOutlined onClick={() => remove(name)}/>}
+                            </Space>)
+                        })}
+                        {/*添加选项按钮*/}
+                        <Form.Item>
+                            <Button type={"link"} onClick={() => add({text: "", value: ""})}
+                            icon={<PlusOutlined/>} block>
+                                添加选项
+                            </Button>
+                        </Form.Item>
+                    </>)}
+                </Form.List>
             </Form.Item>
             <Form.Item name={"isVertical"} valuePropName={"checked"}>
                 <Checkbox>竖向排列</Checkbox>
