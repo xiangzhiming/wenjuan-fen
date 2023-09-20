a9bb0758a10f6997156d8807ec50ae7a9315eae1
向治名 <zhiming.xiang@datavaluable.com>
2023-09-20 15:24:45 +0800

    完成图层组件的功能

--- src/store/componentsReducer/index.ts
+++ src/store/componentsReducer/index.ts
@@ -3,6 +3,7 @@ import {produce} from 'immer';
 import {createSlice, nanoid, PayloadAction} from "@reduxjs/toolkit";
 import {getNextSelectedId, insertNewComponent} from "./utils";
 import cloneDeep from "lodash.clonedeep";
+import {arrayMove} from "@dnd-kit/sortable";
 
 export type ComponentInfoType = {
     fe_id: string  // 返回的组件id
@@ -63,24 +64,24 @@ export const componentsSlice = createSlice({
         removeSelectedComponent: produce((draft: ComponentsStateType) => {
             const {selectId: removedId, componentList} = draft;
             // 删除组件后重新计算默认的选中组件
-            draft.selectId =  getNextSelectedId(removedId, componentList);
+            draft.selectId = getNextSelectedId(removedId, componentList);
             componentList.splice(componentList.findIndex(c => c.fe_id === removedId), 1);  // 删除componentList数组中下标为index的元素
         }),
 
         // 隐藏/显示  组件
         changeComponentHidden: produce(
             (
-            draft: ComponentsStateType,
-            action: PayloadAction<{ fe_id: string, isHidden: boolean }>
+                draft: ComponentsStateType,
+                action: PayloadAction<{ fe_id: string, isHidden: boolean }>
             ) => {
                 const {componentList} = draft;
-                const {fe_id,isHidden} = action.payload;
+                const {fe_id, isHidden} = action.payload;
 
                 let newSelectedId;
                 if (isHidden) {
                     // 要隐藏
                     newSelectedId = getNextSelectedId(fe_id, componentList);
-                }else{
+                } else {
                     // 要显示
                     newSelectedId = fe_id
                 }
@@ -91,10 +92,10 @@ export const componentsSlice = createSlice({
                 if (curComp) {
                     curComp.isHidden = isHidden;
                 }
-        }),
+            }),
 
         // 锁定/解锁  组件
-        toggleComponentLocked: produce((draft: ComponentsStateType,action:PayloadAction<{fe_id: string}>) => {
+        toggleComponentLocked: produce((draft: ComponentsStateType, action: PayloadAction<{ fe_id: string }>) => {
             const {fe_id} = action.payload;
             const curComp = draft.componentList.find(c => c.fe_id === fe_id);
             if (curComp) {
@@ -104,8 +105,8 @@ export const componentsSlice = createSlice({
 
         // 拷贝当前选中的组件
         copySelectedComponent: produce(
-            (draft:ComponentsStateType) => {
-                const {selectId,componentList = []} = draft;
+            (draft: ComponentsStateType) => {
+                const {selectId, componentList = []} = draft;
                 const selectedComponent = componentList.find(c => c.fe_id === selectId) || null;
                 if (selectedComponent === null) return;
                 draft.copiedComponent = cloneDeep(selectedComponent);
@@ -113,7 +114,7 @@ export const componentsSlice = createSlice({
         ),
 
         // 粘贴组件
-        pasteCopiedComponent: produce((draft:ComponentsStateType) => {
+        pasteCopiedComponent: produce((draft: ComponentsStateType) => {
             const {copiedComponent} = draft;
             if (copiedComponent == null) return;
 
@@ -124,21 +125,44 @@ export const componentsSlice = createSlice({
         }),
 
         // 选中上一个
-        selectPrevComponent: produce((draft:ComponentsStateType) => {
-            const {selectId,componentList} = draft;
+        selectPrevComponent: produce((draft: ComponentsStateType) => {
+            const {selectId, componentList} = draft;
             const selectedIndex = componentList.findIndex(c => c.fe_id === selectId);
-            if (selectedIndex < 0 ) return;  // 未选中组件
+            if (selectedIndex < 0) return;  // 未选中组件
             if (selectedIndex <= 0) return; // 已经选中第一个，无法在向上选中
             draft.selectId = componentList[selectedIndex - 1].fe_id
         }),
 
         // 选中下一个
         selectNextComponent: produce((draft: ComponentsStateType) => {
-            const {selectId,componentList} = draft;
+            const {selectId, componentList} = draft;
             const selectedIndex = componentList.findIndex(c => c.fe_id === selectId);
-            if (selectedIndex < 0 ) return;  // 未选中组件
-            if ((selectedIndex + 1)  === componentList.length) return; // 已经选中最后一个，无法在向下选中
+            if (selectedIndex < 0) return;  // 未选中组件
+            if ((selectedIndex + 1) === componentList.length) return; // 已经选中最后一个，无法在向下选中
             draft.selectId = componentList[selectedIndex + 1].fe_id
+        }),
+
+        // 修改 selectedId
+        changeSelectedId: produce((draft: ComponentsStateType, action: PayloadAction<string>) => {
+            draft.selectId = action.payload
+        }),
+
+        // 修改组件标题
+        changeComponentTitle: produce((draft: ComponentsStateType,
+                                       action: PayloadAction<{ fe_id: string, title: string }>) => {
+                const {fe_id, title} = action.payload;
+                const curComp = draft.componentList.find(c => c.fe_id === fe_id);
+                if (curComp) {
+                    curComp.title = title;
+                }
+            }
+        ),
+
+        moveComponent: produce((draft: ComponentsStateType,
+                                action: PayloadAction<{ oldIndex: number, newIndex: number }>) => {
+            const {componentList: curComponentList} = draft;
+            draft.componentList = arrayMove(curComponentList, action.payload.oldIndex,
+                action.payload.newIndex);
         })
 
     }
@@ -147,8 +171,9 @@ export const componentsSlice = createSlice({
 })
 
 export const {
-    resetComponents, changeSelectId, addComponent, changeComponentPops,copySelectedComponent,selectNextComponent,
-    removeSelectedComponent,changeComponentHidden,toggleComponentLocked,pasteCopiedComponent,selectPrevComponent
+    resetComponents, changeSelectId, addComponent, changeComponentPops, copySelectedComponent, selectNextComponent,
+    removeSelectedComponent, changeComponentHidden, toggleComponentLocked, pasteCopiedComponent, selectPrevComponent,
+    changeSelectedId, changeComponentTitle, moveComponent
 } = componentsSlice.actions;
 export default componentsSlice.reducer;
 
