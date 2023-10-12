181b50b10fa627515ffa9b71c246759b67f80baa
向治名 <zhiming.xiang@datavaluable.com>
2023-10-12 15:41:23 +0800

    工具栏中上移按钮功能完成

--- src/pages/question/Edit/EditToolbar.tsx
+++ src/pages/question/Edit/EditToolbar.tsx
@@ -1,10 +1,17 @@
 import {FC} from "react";
 import {Button, Space, Tooltip} from "antd";
-import {BlockOutlined, CopyOutlined, DeleteOutlined, EyeInvisibleOutlined, LockOutlined} from "@ant-design/icons";
+import {
+    BlockOutlined,
+    CopyOutlined,
+    DeleteOutlined,
+    EyeInvisibleOutlined,
+    LockOutlined,
+    UpOutlined
+} from "@ant-design/icons";
 import {useDispatch} from "react-redux";
 import {
     changeComponentHidden,
-    copySelectedComponent, pasteCopiedComponent,
+    copySelectedComponent, moveComponent, pasteCopiedComponent,
     removeSelectedComponent,
     toggleComponentLocked
 } from "../../../store/componentsReducer";
@@ -12,8 +19,14 @@ import useGetComponentInfo from "../../../hooks/useGetComponentInfo";
 
 export const EditToolbar: FC = () => {
     const dispatch = useDispatch();
-    const {selectId,selectedComponent,copiedComponent} = useGetComponentInfo();
+    const {selectId, componentList, selectedComponent, copiedComponent} = useGetComponentInfo();
     const {isLocked} = selectedComponent || {};
+    const length = componentList.length;
+    const selectedIndex = componentList.findIndex(c => c.fe_id === selectId);
+    const isFirst = selectedIndex <= 0; // 表示排在第一个了，没办法上移
+    const isLast = selectedIndex + 1 >= length;  // 表示排在最后一个了，没办法下移了
+
+
     // 删除组件
     function handleDelete() {
         dispatch(removeSelectedComponent());
@@ -39,6 +52,13 @@ export const EditToolbar: FC = () => {
         dispatch(pasteCopiedComponent());
     }
 
+
+    // 上移
+    function moveUp() {
+        if (isFirst) return;
+        dispatch(moveComponent({oldIndex: selectedIndex, newIndex: selectedIndex - 1}))
+    }
+
     // TODO: 撤销/重做  上移/下移
 
     return (<Space>
@@ -50,7 +70,7 @@ export const EditToolbar: FC = () => {
         </Tooltip>
         <Tooltip title={"锁定"}>
             <Button shape={"circle"} icon={<LockOutlined/>} onClick={handleLock}
-            type={isLocked ? "primary" : "default"}/>
+                    type={isLocked ? "primary" : "default"}/>
         </Tooltip>
         <Tooltip title={"复制"}>
             <Button shape={"circle"} icon={<CopyOutlined/>} onClick={copy}/>
@@ -58,6 +78,10 @@ export const EditToolbar: FC = () => {
         <Tooltip title={"粘贴"}>
             <Button shape={"circle"} icon={<BlockOutlined/>} onClick={paste}
                     disabled={copiedComponent == null}/>
+        </Tooltip>
+        <Tooltip title={"上移"}>
+            <Button shape={"circle"} icon={<UpOutlined/>} onClick={moveUp}
+                    disabled={isFirst}/>
         </Tooltip>
     </Space>)
 }
